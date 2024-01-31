ce82a19d196df4e6bfea7272b53fa6cd247694f0
neojarvis <techadmin@iamneo.ai>
2024-01-31 11:10:08 +0000

    Manual-save-commit-1/31/2024--4:40:05-PM

--- main.sql
+++ main.sql
@@ -5,8 +5,8 @@
     );
 
     -- Insert values into the table
-    INSERT INTO ExampleTable (ID, Name) VALUES (1, '123');
-    INSERT INTO ExampleTable (ID, Name) VALUES (2, '456');
-    INSERT INTO ExampleTable (ID, Name) VALUES (3, '789');
+    INSERT INTO ExampleTable (ID, Name) VALUES (1, 'abc');
+    INSERT INTO ExampleTable (ID, Name) VALUES (2, 'def');
+    INSERT INTO ExampleTable (ID, Name) VALUES (3, 'ghf');
 
 -- Select and view all data from the tabl
